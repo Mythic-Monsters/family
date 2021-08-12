f8d9bcfd354ffa56324d61e5ee14b3c51bddb2d7
Mythic-Monsters <73722034+Mythic-Monsters@users.noreply.github.com>
2021-08-12 12:58:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Family Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function nextslide() {
+if (i==5)
+{
+  i=0;
+}
+document.getElementById("p").src=images[i]
+document.getElementById("div1").innerHTML=names[i]
+i++
+}
